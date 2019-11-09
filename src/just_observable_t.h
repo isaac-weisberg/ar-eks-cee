b683ec1affa197e0a8ace767b6505ba92ea2b87d
Isaac Weisberg <isaac_weisberg@outlook.com>
2019-11-09 23:07:11 +0300

    Rework the types names... really confusing

--- src/just_observable_t.h
+++ src/just_observable_t.h
@@ -3,38 +3,38 @@
 
 #include "ceneric_closure.h"
 
+typedef void (*just_disposable_t)(void);
+
+void just_disposable() {
+    
+}
+
 typedef struct {
 
 } just_handler_closure_ctx_int_t;
 
 CENERIC_CLOSURE(void, just_handler_closure_int_t, just_handler_closure_ctx_int_t, int)
 
-typedef void (*just_disposable_int_t)(void);
-
-void just_disposable_int() {
-    
-}
-
 typedef struct {
     int captured;
 } just_subscribe_closure_ctx_int_t;
 
-CENERIC_CLOSURE(just_disposable_int_t, just_subscribe_closure_t, just_subscribe_closure_ctx_int_t, just_handler_closure_int_t)
+CENERIC_CLOSURE(just_disposable_t, just_subscribe_closure_int_t, just_subscribe_closure_ctx_int_t, just_handler_closure_int_t)
 
-just_disposable_int_t just_subscribe_closure_lambda(just_subscribe_closure_ctx_int_t* ctx, just_handler_closure_int_t handler) {
+just_disposable_t just_subscribe_closure_lambda_int(just_subscribe_closure_ctx_int_t* ctx, just_handler_closure_int_t handler) {
     handler.lambda(&handler.ctx, ctx->captured);
-    return just_disposable_int;
+    return just_disposable;
 }
 
 typedef struct {
-    just_subscribe_closure_t subscribe;
+    just_subscribe_closure_int_t subscribe;
 } just_observable_int_t;
 
 just_observable_int_t just_observable_int_init(int value) {
     just_observable_int_t observable;
 
     observable.subscribe.ctx.captured = value;
-    observable.subscribe.lambda = just_subscribe_closure_lambda;
+    observable.subscribe.lambda = just_subscribe_closure_lambda_int;
 
     return observable;
 }
