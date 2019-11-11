1dd495f62ed6194828c46c806f7d8615b74ba8cd
Isaac Weisberg <isaac_weisberg@outlook.com>
2019-11-11 19:49:19 +0300

    Add closure usage for the article

--- src/usage.c
+++ src/usage.c
@@ -17,3 +17,22 @@ void usage() {
 
     call_just_subscribe_closure_int_t(observable.subscribe, handler);
 }
+
+// closure usage
+typedef struct {
+    float captured;
+} add_some_context;
+
+CENERIC_CLOSURE(float, add_some, add_some_context, float)
+
+static float add_some_impl(add_some_context* ctx, float param) {
+    return ctx->captured + param;
+}
+
+void closureUsage() {
+    add_some_context add_35_ctx = { .captured = 35 };
+    add_some add_35_closure = add_some_init(add_35_ctx, add_some_impl);
+
+    float value1 = call_add_some(add_35_closure, 3); // 38
+    float value2 = call_add_some(add_35_closure, -30); // 5
+}
