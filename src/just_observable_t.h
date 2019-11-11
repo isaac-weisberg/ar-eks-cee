098e3690718037bfad06f89fc7bd03a231e41548
Isaac Weisberg <isaac_weisberg@outlook.com>
2019-11-11 18:47:58 +0300

    Improve the readability of the just observable code

--- src/just_observable_t.h
+++ src/just_observable_t.h
@@ -10,31 +10,56 @@ void just_disposable() {
 }
 
 #define CENERIC_JUST_OBSERVABLE(EVENT_TYPE_NAME) \
-typedef struct { \
 \
-} just_handler_closure_ctx_##EVENT_TYPE_NAME##_t; \
 \
-CENERIC_CLOSURE(void, just_handler_closure_##EVENT_TYPE_NAME##_t, just_handler_closure_ctx_##EVENT_TYPE_NAME##_t, EVENT_TYPE_NAME) \
+typedef struct { } just_handler_closure_ctx_##EVENT_TYPE_NAME##_t; \
+\
+\
+CENERIC_CLOSURE( \
+    void, \
+    just_handler_closure_##EVENT_TYPE_NAME##_t, \
+    just_handler_closure_ctx_##EVENT_TYPE_NAME##_t, \
+    EVENT_TYPE_NAME \
+) \
+\
 \
 typedef struct { \
     EVENT_TYPE_NAME captured; \
 } just_subscribe_closure_ctx_##EVENT_TYPE_NAME##_t; \
 \
-CENERIC_CLOSURE(just_disposable_t, just_subscribe_closure_##EVENT_TYPE_NAME##_t, just_subscribe_closure_ctx_##EVENT_TYPE_NAME##_t, just_handler_closure_##EVENT_TYPE_NAME##_t) \
 \
-just_disposable_t just_subscribe_closure_lambda_##EVENT_TYPE_NAME(just_subscribe_closure_ctx_##EVENT_TYPE_NAME##_t* ctx, just_handler_closure_##EVENT_TYPE_NAME##_t handler) { \
+CENERIC_CLOSURE( \
+    just_disposable_t, \
+    just_subscribe_closure_##EVENT_TYPE_NAME##_t, \
+    just_subscribe_closure_ctx_##EVENT_TYPE_NAME##_t, \
+    just_handler_closure_##EVENT_TYPE_NAME##_t \
+) \
+\
+\
+just_disposable_t \
+just_subscribe_closure_lambda_##EVENT_TYPE_NAME( \
+    just_subscribe_closure_ctx_##EVENT_TYPE_NAME##_t* ctx, \
+    just_handler_closure_##EVENT_TYPE_NAME##_t handler \
+) { \
     call_just_handler_closure_##EVENT_TYPE_NAME##_t(handler, ctx->captured); \
     return just_disposable; \
 } \
+\
 \
 typedef struct { \
     just_subscribe_closure_##EVENT_TYPE_NAME##_t subscribe; \
 } just_observable_##EVENT_TYPE_NAME##_t; \
 \
-just_observable_##EVENT_TYPE_NAME##_t just_observable_##EVENT_TYPE_NAME##_init(EVENT_TYPE_NAME value) { \
+\
+just_observable_##EVENT_TYPE_NAME##_t \
+just_observable_##EVENT_TYPE_NAME##_init( \
+    EVENT_TYPE_NAME value \
+) { \
     just_observable_##EVENT_TYPE_NAME##_t observable; \
 \
-    observable.subscribe = just_subscribe_closure_##EVENT_TYPE_NAME##_t_init((just_subscribe_closure_ctx_##EVENT_TYPE_NAME##_t){ value }, just_subscribe_closure_lambda_##EVENT_TYPE_NAME); \
+    observable.subscribe = just_subscribe_closure_##EVENT_TYPE_NAME##_t_init( \
+        (just_subscribe_closure_ctx_##EVENT_TYPE_NAME##_t){ value }, \
+        just_subscribe_closure_lambda_##EVENT_TYPE_NAME); \
 \
     return observable; \
 } \
