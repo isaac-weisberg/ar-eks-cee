a1a38ad2d0141b0daf64b5bc55f796e5bd235c8b
Isaac Weisberg <isaac_weisberg@outlook.com>
2019-11-11 20:26:12 +0300

    Separate the ctx creation in the usage

--- src/usage.c
+++ src/usage.c
@@ -10,8 +10,10 @@ void handleThemInts(just_handler_closure_ctx_int_t* ctx, int event) {
 void usage() {
     just_observable_int_t observable = just_observable_int_init(4);
 
+    just_handler_closure_ctx_int_t ctx = {};
+
     just_handler_closure_int_t handler = just_handler_closure_int_t_init(
-        (just_handler_closure_ctx_int_t){}, 
+        ctx,
         handleThemInts
     );
 
